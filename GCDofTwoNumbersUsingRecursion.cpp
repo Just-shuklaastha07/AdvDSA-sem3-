d24cdaabc8b20ee005457d09bf4f20b6bcb45e18
Just-shuklaastha07 <shuklaastha07@gmail.com>
2026-09-21 09:50:43 +0530

    GCD of Two numbers using recursion

---
+++ GCDofTwoNumbersUsingRecursion.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int GCD(int a, int b)
+{
+    if(b == 0)
+    {
+        return a;
+    }
+
+    return GCD(b, a % b);
+}
+
+int main()
+{
+    int a, b;
+
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    int result = GCD(a, b);
+
+    cout << "GCD = " << result;
+
+    return 0;
+}
